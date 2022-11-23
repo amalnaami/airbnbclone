425552824b8d3c582365c0c8d2978c70bc74fbbe
amalnaami <amalahmadnaami97@gmail.com>
2022-11-23 14:42:41 +0200

    Define prefHelper functions

--- lib/data/prefs_helper/prefs_helper.dart
+++ lib/data/prefs_helper/prefs_helper.dart
@@ -1,23 +1,32 @@
+import 'package:airbnbclone/core/contstant/shared_preferences.dart';
+import 'package:airbnbclone/core/localization/app_language.dart';
 import 'package:airbnbclone/data/prefs_helper/i_prefs_helper.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 
 /// Prefs Helper contains the implementation of all Shared Preferences functions
 class PrefsHelper extends IPrefsHelper {
   @override
-  Future<int> getAppLanguage() {
-    // TODO: implement getAppLanguage
-    throw UnimplementedError();
+  Future<int> getAppLanguage() async {
+    return (await getSharedPreferences())
+            .getInt(SharedPreferencesConstants.appLanguage) ??
+        AppLanguageKeys.EN;
   }
 
   @override
   Future<SharedPreferences> getSharedPreferences() {
-    // TODO: implement getSharedPreferences
-    throw UnimplementedError();
+    return SharedPreferences.getInstance();
   }
 
   @override
-  Future<bool> setAppLanguage(int value) {
-    // TODO: implement setAppLanguage
-    throw UnimplementedError();
+  Future<bool> setAppLanguage(int value) async {
+    try {
+      await (await getSharedPreferences()).setInt(
+        SharedPreferencesConstants.appLanguage,
+        value,
+      );
+      return true;
+    } catch (e) {
+      return false;
+    }
   }
 }
