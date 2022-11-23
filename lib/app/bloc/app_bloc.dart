b12677e70633b87784af34d6749afdb7a45b6ec1
amalnaami <amalahmadnaami97@gmail.com>
2022-11-23 14:46:24 +0200

    Define the _tryGetAppLanguage body

--- lib/app/bloc/app_bloc.dart
+++ lib/app/bloc/app_bloc.dart
@@ -1,5 +1,6 @@
 import 'dart:async';
 
+import 'package:airbnbclone/core/localization/app_language.dart';
 import 'package:airbnbclone/data/prefs_helper/prefs_helper.dart';
 import 'package:equatable/equatable.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
@@ -7,14 +8,20 @@ import 'package:flutter_bloc/flutter_bloc.dart';
 part 'app_event.dart';
 part 'app_state.dart';
 
-///
+/// the bloc class to controller the app state
 class AppBloc extends Bloc<AppEvent, AppState> {
-  ///
+  /// constructor
   AppBloc(this._prefsHelper) : super(const AppState.init());
 
   final PrefsHelper _prefsHelper;
 
+  /// Function to retrieve the app language from SharedPreferences
   Future<int> _tryGetAppLanguage() async {
-    return 0;
+    try {
+      final appLanguage = await _prefsHelper.getAppLanguage();
+      return appLanguage;
+    } catch (_) {
+      return AppLanguageKeys.EN;
+    }
   }
 }
